3deb9b2ca940507e87a6c931852ab1b948bde05c
Danil Goncharov <daya.danya@inbox.ru>
2024-02-02 22:39:31 +0300

    написал unit-test для save-метода MeasurementUseCase

--- src/test/java/org/ylab/usecases/MeasurementUseCaseTest.java
+++ src/test/java/org/ylab/usecases/MeasurementUseCaseTest.java
@@ -1,57 +1,98 @@
 package org.ylab.usecases;
 
-class MeasurementUseCaseTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+import org.ylab.domain.models.Measurement;
+import org.ylab.exceptions.BadMeasurementAmountException;
+import org.ylab.exceptions.CounterNotFoundException;
+import org.ylab.exceptions.WrongDateException;
+import org.ylab.repositories.MeasurementRepo;
 
-//    MeasurementUseCase measurementUseCase;
-//
-//    @BeforeEach
-//    void setUp() {
-////        CounterTypeUseCase counterTypeUseCase = new CounterTypeUseCase();
-////        counterTypeUseCase.save(new CounterType("cold"));
-////        CounterUseCase counterUseCase = new CounterUseCase(counterTypeUseCase);
-////        counterUseCase.save(new Counter(1, new CounterType("cold")));
-////        measurementUseCase = new MeasurementUseCase(counterUseCase);
-//
-//
-//    }
-//    @Test
-//    void save(){
-//        MeasurementInDto dto = new MeasurementInDto(5345.5, "cold");
-//        Measurement measurement = new Measurement(dto, LocalDateTime.now());
-//        measurementUseCase.save(measurement, 1);
-//        Assertions.assertThrows(WrongDateException.class, () -> measurementUseCase.save(measurement, 1));
-//    }
-//
-//    @Test
-//    void findAll() {
-//    }
-//
-//    @Test
-//    void findAllById() {
-//    }
-//
-//    @Test
-//    void findLast() {
-//        MeasurementInDto dto = new MeasurementInDto(5345.5, "cold");
-//        Measurement measurement1 = new Measurement(dto, LocalDateTime.of(
-//                2023, 1, 25, 13, 7, 23));
-//        Measurement measurement2 = new Measurement(dto, LocalDateTime.of(
-//                2023, 3, 25, 13, 7, 23));
-//        Measurement measurement3 = new Measurement(dto, LocalDateTime.of(
-//                2023, 4, 25, 13, 7, 23));
-//        measurementUseCase.save(measurement1, 1);
-//        measurementUseCase.save(measurement2, 1);
-//        measurementUseCase.save(measurement3, 1);
-//        Assertions.assertEquals(measurement3, measurementUseCase.findByMonth(1, 4, new CounterType("cold")));
-//    }
-//
-//    @Test
-//    void findByMonth() {
-//        MeasurementInDto dto = new MeasurementInDto(5345.5, "cold");
-//        Measurement measurement = new Measurement(dto, LocalDateTime.of(
-//                2024, 7, 25, 13, 7, 23));
-//        measurementUseCase.save(measurement, 1);
-//        Assertions.assertEquals(measurement, measurementUseCase.findByMonth(1, 7, new CounterType("cold")));
-//
-//    }
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.Optional;
+
+@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
+class MeasurementUseCaseTest {
+    @InjectMocks
+    MeasurementUseCase measurementUseCase;
+    @Mock
+    MeasurementRepo measurementRepo;
+
+    @Mock
+    CounterUseCase counterUseCase;
+
+    @Mock
+    OperationUseCase operationUseCase;
+
+    @Test
+    void save_ifCounterNotExists_throwsException() {
+        LocalDateTime localDateTime = LocalDateTime.of(
+                2023, Month.DECEMBER, 25, 10, 15, 1
+        );
+        Measurement measurement = new Measurement(1, 1000.1, localDateTime,
+                "HOT", 2);
+        Mockito.when(counterUseCase.findIdByPersonIdAndCounterType(3,
+                        measurement.getCounterType())).thenReturn(Optional.empty());
+        Assertions.assertThrows(CounterNotFoundException.class,
+                () -> measurementUseCase.save(measurement, 3));
+    }
+    @Test
+    void save_incorrectDate_throwsException() {
+        LocalDateTime localDateTime = LocalDateTime.of(
+                2023, Month.DECEMBER, 25, 10, 15, 1
+        );
+        Measurement measurement = new Measurement(1, 1000.1, localDateTime,
+                "HOT", 2);
+        Mockito.when(counterUseCase.findIdByPersonIdAndCounterType(3,
+                measurement.getCounterType())).thenReturn(Optional.of(2L));
+        Mockito.when(measurementRepo.findLastDate(2L))
+                        .thenReturn(Optional.of(LocalDateTime.now()));
+        Assertions.assertThrows(WrongDateException.class,
+                () -> measurementUseCase.save(measurement, 3));
+    }
+    @Test
+    void save_smallerAmount_throwsException() {
+        LocalDateTime localDateTime = LocalDateTime.of(
+                2023, Month.DECEMBER, 25, 10, 15, 1
+        );
+        Measurement measurement = new Measurement(1, 1000.1, LocalDateTime.now(),
+                "HOT", 2);
+        Mockito.when(counterUseCase.findIdByPersonIdAndCounterType(3,
+                measurement.getCounterType())).thenReturn(Optional.of(2L));
+        Mockito.when(measurementRepo.findLastDate(2L))
+                .thenReturn(Optional.of(localDateTime));
+        Mockito.when(measurementRepo.findLast(2L))
+                        .thenReturn(Optional.of(new Measurement(1, 1000.2, LocalDateTime.now(),
+                                "HOT", 2)));
+        Assertions.assertThrows(BadMeasurementAmountException.class,
+                () -> measurementUseCase.save(measurement, 3));
+    }
+    //todo other tests
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findAllById() {
+    }
+
+    @Test
+    void findLast() {
+
+    }
+
+    @Test
+    void findByMonth() {
+
+
+    }
 }
